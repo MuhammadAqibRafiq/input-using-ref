36f4eedeae66137b8b8aedbf9f2c1441ad3c6476
Muhammad Aqib Rafiq <aqibrafiq001@outlook.com>
2022-01-28 00:21:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import './App.css';
 
+
 function App() {
+
+  const [file, setFile] = useState("")
+
+
+  console.log(file && file[0].name)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Row className="p-2">
+
+       <Col md={2} sm={1} className='col1'>
+          Choose File
+       </Col>
+
+       <Col md={4} sm={2} className='col2'>
+        <input className='col2-input' defaultValue={file && file[0].name}/>
+       </Col>
+       
+       <Col md={2} sm={2} className='col1'>
+          Submit
+       </Col>
+
+     </Row> */}
+
+      <div className='new'>
+
+
+        <div className='input-btn-choosefile'>
+          Choose File
+        </div>
+
+        <div className='input-placeholder'>
+          <input style={{background:"transparent" , width:"100%"}} />
+        </div>
+
+
+
+        {/* <button>submit</button> */}
+      </div>
+
+
+      <input type="file" onChange={e => setFile(e.target.files)} />
+    </>
   );
 }
 
